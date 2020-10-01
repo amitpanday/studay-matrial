84c7afd0c0a7aa1440c1e8f348789cb781490b5f
Gayatri19670 <samirgupta19670@gmail.com>
2020-10-01 10:19:18 +0530

    first commit

---
+++ JS/Script.js
@@ -0,0 +1,48 @@
+console.log("Calculator")
+
+// let clear_btn = document.getElementsByClassName('clear_btn').addEventListener("click")
+// document.getElementsByClassName('clear_btn').addEventListener("click", function(e) {
+//     console.log("You have clicked the heading");
+
+// });
+
+let addBtn = document.getElementById("key-btn-1");
+addBtn.addEventListener("click", function(e) {
+    console.log(1);
+});
+let addBtn = document.getElementById("key-btn-2");
+addBtn.addEventListener("click", function(e) {
+    console.log(2);
+});
+let addBtn = document.getElementById("key-btn-3");
+addBtn.addEventListener("click", function(e) {
+    console.log(3);
+});
+let addBtn = document.getElementById("key-btn-4");
+addBtn.addEventListener("click", function(e) {
+    console.log(4);
+});
+let addBtn = document.getElementById("key-btn-5");
+addBtn.addEventListener("click", function(e) {
+    console.log(5);
+});
+let addBtn = document.getElementById("key-btn-6");
+addBtn.addEventListener("click", function(e) {
+    console.log(6);
+});
+let addBtn = document.getElementById("key-btn-7");
+addBtn.addEventListener("click", function(e) {
+    console.log(7);
+});
+let addBtn = document.getElementById("key-btn-8");
+addBtn.addEventListener("click", function(e) {
+    console.log(8);
+});
+let addBtn = document.getElementById("key-btn-9");
+addBtn.addEventListener("click", function(e) {
+    console.log(9);
+});
+let addBtn = document.getElementById("key-btn-0");
+addBtn.addEventListener("click", function(e) {
+    console.log(0);
+});
