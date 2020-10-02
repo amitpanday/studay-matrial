504d59704c65288749fe0889f507c8372c290d89
amitpanday <amitp381997@gmail.com>
2020-10-02 13:44:05 +0530

    js code

---
+++ JS/Script.js
@@ -0,0 +1,85 @@
+const btn1 = document.getElementById('btn1').value;
+const btn2 = document.getElementById('btn2').value;
+const btn3 = document.getElementById('btn3').value;
+const btn4 = document.getElementById('btn4').value;
+const btn5 = document.getElementById('btn5').value;
+const btn6 = document.getElementById('btn6').value;
+const btn7 = document.getElementById('btn7').value;
+const btn8 = document.getElementById('btn8').value;
+const btn9 = document.getElementById('btn9').value;
+const btn10 = document.getElementById('btn10').value;
+const btn11 = document.getElementById('btn11').value;
+const btn12 = document.getElementById('btn12').value;
+const btn13 = document.getElementById('btn13').value;
+const btn14 = document.getElementById('btn14').value;
+const btn15 = document.getElementById('btn15').value;
+const btn16 = document.getElementById('btn16').value;
+const btn17 = document.getElementById('btn17').value;
+const btn18 = document.getElementById('btn18').value;
+const btn19 = document.getElementById('btn19').value;
+const btn20 = document.getElementById('btn20').value;
+
+const screen = document.getElementById('screen');
+
+
+function onClickButton(val) {
+    console.log(val);
+    // screen.innerHtml = `<p>${val}</p>`;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// // let clear_btn = document.getElementsByClassName('clear_btn').
+
+// let addBtn = document.getElementById("btn1");
+// addBtn.addEventListener("click", function(e) {
+//     console.log(1);
+// });
+// let addBtn = document.getElementById("key-btn-2");
+// addBtn.addEventListener("click", function(e) {
+//     console.log(2);
+// });
+// let addBtn = document.getElementById("key-btn-3");
+// addBtn.addEventListener("click", function(e) {
+//     console.log(3);
+// });
+// let addBtn = document.getElementById("key-btn-4");
+// addBtn.addEventListener("click", function(e) {
+//     console.log(4);
+// });
+// let addBtn = document.getElementById("key-btn-5");
+// addBtn.addEventListener("click", function(e) {
+//     console.log(5);
+// });
+// let addBtn = document.getElementById("key-btn-6");
+// addBtn.addEventListener("click", function(e) {
+//     console.log(6);
+// });
+// let addBtn = document.getElementById("key-btn-7");
+// addBtn.addEventListener("click", function(e) {
+//     console.log(7);
+// });
+// let addBtn = document.getElementById("key-btn-8");
+// addBtn.addEventListener("click", function(e) {
+//     console.log(8);
+// });
+// let addBtn = document.getElementById("key-btn-9");
+// addBtn.addEventListener("click", function(e) {
+//     console.log(9);
+// });
+// let addBtn = document.getElementById("key-btn-0");
+// addBtn.addEventListener("click", function(e) {
+//     console.log(0);
+// });
