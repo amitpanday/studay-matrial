627730ebe1214902c7ba3451ae86749c4bf01ea0
amitpanday <amitp381997@gmail.com>
2020-10-03 02:07:04 +0530

    js logic

--- JS/Script.js
+++ JS/Script.js
@@ -1,85 +1,60 @@
-const btn1 = document.getElementById('btn1').value;
-const btn2 = document.getElementById('btn2').value;
-const btn3 = document.getElementById('btn3').value;
-const btn4 = document.getElementById('btn4').value;
-const btn5 = document.getElementById('btn5').value;
-const btn6 = document.getElementById('btn6').value;
-const btn7 = document.getElementById('btn7').value;
-const btn8 = document.getElementById('btn8').value;
-const btn9 = document.getElementById('btn9').value;
-const btn10 = document.getElementById('btn10').value;
-const btn11 = document.getElementById('btn11').value;
-const btn12 = document.getElementById('btn12').value;
-const btn13 = document.getElementById('btn13').value;
-const btn14 = document.getElementById('btn14').value;
-const btn15 = document.getElementById('btn15').value;
-const btn16 = document.getElementById('btn16').value;
-const btn17 = document.getElementById('btn17').value;
-const btn18 = document.getElementById('btn18').value;
-const btn19 = document.getElementById('btn19').value;
-const btn20 = document.getElementById('btn20').value;
-
 const screen = document.getElementById('screen');
+let value1 = ''; // false;
+let value2 = ''; //false;
+let operation = '+';
+// let operPressed = false;
+// let total = 0;
 
 
-function onClickButton(val) {
-    console.log(val);
-    // screen.innerHtml = `<p>${val}</p>`;
+function onNumClick(value) {
+    value = value.toString();
+    // if(operPressed){
+    //     if (!value2) value2 = '';
+    //     value2 = value2 + value;
+    //     screen.innerHTML =value1 + operation + value2;
+    // }else{
+    //     if (!value1) value1 = '';
+    //     value1 = value1 + value;
+    //     screen.innerHTML = value1;
+    // }
+    value1 = value1 + value;
+        screen.innerHTML = value1;
+    console.log(value);
 }
 
+function onOprationClick (value) {
+    // operPressed = true;
+    // operation = value;
+    // let privousValue = screen.innerHTML.toString();
+    // screen.innerHTML = privousValue + value;
+    console.log(value);
+}
 
+function totalValue (){
+    // if (value1 && value2) {
+	// 	total = calculateTotalValue(parseInt(value1), parseInt(value2), operation);
+	// }
+	// total = total.toString();
+    // screen.innerHTML = total;
+    console.log(value1, value2, operation);
+}
 
-
-
-
-
-
-
-
-
-
-
-
-
-// // let clear_btn = document.getElementsByClassName('clear_btn').
-
-// let addBtn = document.getElementById("btn1");
-// addBtn.addEventListener("click", function(e) {
-//     console.log(1);
-// });
-// let addBtn = document.getElementById("key-btn-2");
-// addBtn.addEventListener("click", function(e) {
-//     console.log(2);
-// });
-// let addBtn = document.getElementById("key-btn-3");
-// addBtn.addEventListener("click", function(e) {
-//     console.log(3);
-// });
-// let addBtn = document.getElementById("key-btn-4");
-// addBtn.addEventListener("click", function(e) {
-//     console.log(4);
-// });
-// let addBtn = document.getElementById("key-btn-5");
-// addBtn.addEventListener("click", function(e) {
-//     console.log(5);
-// });
-// let addBtn = document.getElementById("key-btn-6");
-// addBtn.addEventListener("click", function(e) {
-//     console.log(6);
-// });
-// let addBtn = document.getElementById("key-btn-7");
-// addBtn.addEventListener("click", function(e) {
-//     console.log(7);
-// });
-// let addBtn = document.getElementById("key-btn-8");
-// addBtn.addEventListener("click", function(e) {
-//     console.log(8);
-// });
-// let addBtn = document.getElementById("key-btn-9");
-// addBtn.addEventListener("click", function(e) {
-//     console.log(9);
-// });
-// let addBtn = document.getElementById("key-btn-0");
-// addBtn.addEventListener("click", function(e) {
-//     console.log(0);
-// });
+// function calculateTotalValue(value1, value2, operation){
+//     switch (operation) {
+// 		case '+':
+// 			total = value1 + value2;
+// 			break;
+// 		case '-':
+// 			total = value1 - value2;
+// 			break;
+// 		case '/':
+// 			total = value1 / value2;
+// 			break;
+// 		case '*':
+// 			total = value1 * value2;
+// 			break;
+// 		default:
+// 			return false;
+// 	}
+// 	return total;
+// }
