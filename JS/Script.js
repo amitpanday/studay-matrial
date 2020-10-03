ec8bb88787972ba00057ca6046ec64724e0c5dec
amitpanday <amitp381997@gmail.com>
2020-10-03 14:26:11 +0530

    improve

--- JS/Script.js
+++ JS/Script.js
@@ -2,59 +2,49 @@ const screen = document.getElementById('screen');
 let value1 = ''; // false;
 let value2 = ''; //false;
 let operation = '+';
-// let operPressed = false;
-// let total = 0;
+let operPressed = false;
+let total = 0;
 
 
 function onNumClick(value) {
     value = value.toString();
-    // if(operPressed){
-    //     if (!value2) value2 = '';
-    //     value2 = value2 + value;
-    //     screen.innerHTML =value1 + operation + value2;
-    // }else{
-    //     if (!value1) value1 = '';
-    //     value1 = value1 + value;
-    //     screen.innerHTML = value1;
-    // }
-    value1 = value1 + value;
+    if(operPressed){
+        value2 = value2 + value;
+        screen.innerHTML =value1 + operation + value2;
+    }else{
+        value1 = value1 + value;
         screen.innerHTML = value1;
+    }
     console.log(value);
 }
 
 function onOprationClick (value) {
-    // operPressed = true;
-    // operation = value;
-    // let privousValue = screen.innerHTML.toString();
-    // screen.innerHTML = privousValue + value;
-    console.log(value);
+    value = value.toString();
+    operPressed = true;
+    operation = value;
+    let privousValue = screen.innerHTML.toString();
+    screen.innerHTML = privousValue + value;
+    console.log(value,operPressed);
 }
 
 function totalValue (){
-    // if (value1 && value2) {
-	// 	total = calculateTotalValue(parseInt(value1), parseInt(value2), operation);
-	// }
-	// total = total.toString();
-    // screen.innerHTML = total;
-    console.log(value1, value2, operation);
+    switch (operation) {
+		case '+':
+			total = parseInt(value1) + parseInt(value2);
+			break;
+		case '-':
+			total = parseInt(value1) - parseInt(value2);
+			break;
+		case '/':
+			total = parseInt(value1) / parseInt(value2);
+			break;
+		case '*':
+			total = parseInt(value1) * parseInt(value2);
+			break;
+		default:
+			return false;
+	}
+	total = total.toString();
+    screen.innerHTML = total;
+    console.log(value1,operation, value2,'total =>',total);
 }
-
-// function calculateTotalValue(value1, value2, operation){
-//     switch (operation) {
-// 		case '+':
-// 			total = value1 + value2;
-// 			break;
-// 		case '-':
-// 			total = value1 - value2;
-// 			break;
-// 		case '/':
-// 			total = value1 / value2;
-// 			break;
-// 		case '*':
-// 			total = value1 * value2;
-// 			break;
-// 		default:
-// 			return false;
-// 	}
-// 	return total;
-// }
