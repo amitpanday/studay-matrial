73832f5e4b25f19269641e4a8d5e358f6c95ca46
Gayatri19670 <samirgupta19670@gmail.com>
2020-10-03 12:04:03 +0530

    first commit

--- JS/Script.js
+++ JS/Script.js
@@ -1,54 +1,50 @@
-// console.log("Calculator")
+const btn1 = document.getElementById('btn1').value;
+const btn2 = document.getElementById('btn2').value;
+const btn3 = document.getElementById('btn3').value;
+const btn4 = document.getElementById('btn4').value;
+const btn5 = document.getElementById('btn5').value;
+const btn6 = document.getElementById('btn6').value;
+const btn7 = document.getElementById('btn7').value;
+const btn8 = document.getElementById('btn8').value;
+const btn9 = document.getElementById('btn9').value;
+const btn10 = document.getElementById('btn10').value;
+const btn11 = document.getElementById('btn11').value;
+const btn12 = document.getElementById('btn12').value;
+const btn13 = document.getElementById('btn13').value;
+const btn14 = document.getElementById('btn14').value;
+const btn15 = document.getElementById('btn15').value;
+const btn16 = document.getElementById('btn16').value;
+const btn17 = document.getElementById('btn17').value;
+const btn18 = document.getElementById('btn18').value;
+const btn19 = document.getElementById('btn19').value;
+const btn20 = document.getElementById('btn20').value;
 
-// // let clear_btn = document.getElementsByClassName('clear_btn').addEventListener("click")
-// // document.getElementsByClassName('clear_btn').addEventListener("click", function(e) {
-// //     console.log("You have clicked the heading");
-
-// // });
-
-// let addBtn = document.getElementById("key-btn-1");
-// addBtn.addEventListener("click", function(e) {
-//     console.log(1);
-// });
-// let addBtn = document.getElementById("key-btn-2");
-// addBtn.addEventListener("click", function(e) {
-//     console.log(2);
-// });
-// let addBtn = document.getElementById("key-btn-3");
-// addBtn.addEventListener("click", function(e) {
-//     console.log(3);
-// });
-// let addBtn = document.getElementById("key-btn-4");
-// addBtn.addEventListener("click", function(e) {
-//     console.log(4);
-// });
-// let addBtn = document.getElementById("key-btn-5");
-// addBtn.addEventListener("click", function(e) {
-//     console.log(5);
-// });
-// let addBtn = document.getElementById("key-btn-6");
-// addBtn.addEventListener("click", function(e) {
-//     console.log(6);
-// });
-// let addBtn = document.getElementById("key-btn-7");
-// addBtn.addEventListener("click", function(e) {
-//     console.log(7);
-// });
-// let addBtn = document.getElementById("key-btn-8");
-// addBtn.addEventListener("click", function(e) {
-//     console.log(8);
-// });
-// let addBtn = document.getElementById("key-btn-9");
-// addBtn.addEventListener("click", function(e) {
-//     console.log(9);
-// });
-// let addBtn = document.getElementById("key-btn-0");
-// addBtn.addEventListener("click", function(e) {
-//     console.log(0);
-// });
-
+// let screen = document.getElementById('screen');
+// screen = document.setAttribute('screen', val)
 
 //function that display value 
-function dis(val) {
-    document.getElementById("result").value += val
+function onClickButton(val) {
+    // console.log(val);
+    let op = document.getElementById("screen").innerHTML += val;
+    document.getElementById("screen").innerHTML = op;
+    //     // document.getElementById('screen').value += val
+    //     // document.setAttribute('screen', `&{val}`);
+    //     let screen = (`&{val}`);
+    //     console.log(screen);
+    // 
+}
+
+//function that display value 
+// function onClickButton(val) {
+
+// }
+
+//function that clear the display 
+function onClickButton1() {
+    document.getElementById("screen").innerHTML = ""
+}
+
+//function that delete the no
+function onClickButton2() {
+    document.getElementById("screen").innerHTML = val - val;
 }
