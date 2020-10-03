e480169aefca349d4735f58ec6d36a957fa4f5d3
Ritika Bansal <ritikabansal5678@gmail.com>
2020-10-03 23:45:43 +0530

    calculator

--- js/script.js
+++ js/script.js
@@ -1,40 +1,50 @@
-const btn1 = document.getElementById('btn1');
-const btn2 = document.getElementById('btn2');
-const btn3 = document.getElementById('btn3');
-const btn4 = document.getElementById('btn4');
-const btn5 = document.getElementById('btn5');
-const btn6 = document.getElementById('btn6');
-const btn7 = document.getElementById('btn7');
-const btn8 = document.getElementById('btn8');
-const btn9 = document.getElementById('btn9');
-const btn10 = document.getElementById('btn10');
-const btn11 = document.getElementById('btn11');
-const btn12 = document.getElementById('btn12');
-const btn13 = document.getElementById('btn13');
-const btn14 = document.getElementById('btn14');
-const btn15 = document.getElementById('btn15');
-const btn16 = document.getElementById('btn16');
-const btn17 = document.getElementById('btn17');
-const btn18 = document.getElementById('btn18');
-const btn19 = document.getElementById('btn19');
-const btn20 = document.getElementById('btn20');
+const screen = document.getElementById('screen');
+let value1 = ''; // false;
+let value2 = ''; //false;
+let operation = '+';
+let operPressed = false;
+let total = 0;
 
-// let btn = document.getElementById('btn');
-// btn.addEventListener('click',func1);
-// function func1(e){
-//     console.log(e.target.value,e);
-// }
 
-// document.getElementById("btn").addEventListener("click",function(e){
-//     console.log(e.target.value,e);
-// });
+function onNumClick(value) {
+    value = value.toString();
+    if(operPressed){
+        value2 = value2 + value;
+        screen.innerHTML =value1 + operation + value2;
+    }else{
+        value1 = value1 + value;
+        screen.innerHTML = value1;
+    }
+    console.log(value);
+}
 
-// let btn1 = document.getElementById('btn1');
-// btn1.addEventListener('click',func2);
-// function func2(e){
-//     console.log(e.target.value,e);
-// }
+function onOprationClick (value) {
+    value = value.toString();
+    operPressed = true;
+    operation = value;
+    let privousValue = screen.innerHTML.toString();
+    screen.innerHTML = privousValue + value;
+    console.log(value,operPressed);
+}
 
-// document.getElementById("btn1").addEventListener("click",function(e){
-//     console.log(e.target.value,e);
-// });
+function totalValue (){
+    switch (operation) {
+		case '+':
+			total = parseInt(value1) + parseInt(value2);
+			break;
+		case '-':
+			total = parseInt(value1) - parseInt(value2);
+			break;
+		case '/':
+			total = parseInt(value1) / parseInt(value2);
+			break;
+		case '*':
+			total = parseInt(value1) * parseInt(value2);
+			break;
+		default:
+			return false;
+	}
+	total = total.toString();
+    screen.innerHTML = total;
+    console.log(value1,operation, value2,'total =>',total);
+}
